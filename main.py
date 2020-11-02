68f0e78d32fd96f527eb389a13ed41a69e1440dd
Juliana Guamá <juliana.guama@gmail.com>
2020-11-01 23:02:32 -0300

    Add host and port config on main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__) #->
 def main():
     return "Hello World!"
 
-
 if __name__ == '__main__':
-    app.run()
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '3000'))
+    except ValueError:
+        PORT = 3000
+    app.run(HOST, PORT)
